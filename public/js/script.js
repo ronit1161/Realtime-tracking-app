b98005e8283bbca1c949f968b498b0633d02134d
Ronit <ronittambe116@gmail.com>
2024-07-15 16:00:15 +0530

    sec commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,47 +1,44 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const socket = io();
+document.addEventListener("DOMContentLoaded", () => {
+  const socket = io();
 
-    if(navigator.geolocation){
-        navigator.geolocation.watchPosition((position) => {
-            const { latitude, longitude } = position.coords;
-            socket.emit("send-location", { latitude, longitude });
-        },
-    );
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      socket.emit("send-location", { latitude, longitude });
+    });
     (error) => {
-        console.error(error);
+      console.error(error);
     },
-    {
+      {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0,
+      };
+  }
+
+  const map = L.map("map").setView([0, 0], 16);
+
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "OpenStreetMap by Ronit",
+  }).addTo(map);
+
+  const markers = {};
+
+  socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude]);
+
+    if (markers[id]) {
+      markers[id].setLatLng([latitude, longitude]);
+    } else {
+      markers[id] = L.marker([latitude, longitude]).addTo(map);
     }
-    };
-
-    const map = L.map("map").setView([0,0], 16);
-
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: "OpenStreetMap by Ronit"
-    }).addTo(map)
-
-    const markers = {};
-
-    socket.on("receive-location", (data) => {
-        const { id, latitude, longitude } = data;
-        map.setView([ latitude, longitude ]);
-
-        if(markers[id]){
-            markers[id].setLatLng([ latitude, longitude ]);
-        }
-        else{
-            markers[id] = L.marker([ latitude, longitude ]).addTo(map)
-        }
-    })
-
-    socket.on("user-disconnectedt", (id) => {
-        if(marker[id]) {
-            map.removeLayer(markers[id]);
-            delete markers[id];
-        }
-    })
-
   });
+
+  socket.on("user-disconnectedt", (id) => {
+    if (marker[id]) {
+      map.removeLayer(markers[id]);
+      delete markers[id];
+    }
+  });
+});
